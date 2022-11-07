0a466c4c31b5f9c2cabe4128f0e8eed9cefa53dc
Kamlesh Singh <tksguide8@gmail.com>
2022-11-07 14:16:03 +0530

    pcsm admin panel 2nd commit

--- src/pages/DashboardApp.js
+++ src/pages/DashboardApp.js
@@ -1,27 +1,19 @@
-import { faker } from '@faker-js/faker';
-// @mui
-import { useTheme } from '@mui/material/styles';
 import { Grid, Container, Typography } from '@mui/material';
 // components
 import Page from '../components/Page';
 import Iconify from '../components/Iconify';
 // sections
 import {
-  AppTasks,
-  AppNewsUpdate,
-  AppOrderTimeline,
   AppCurrentVisits,
   AppWebsiteVisits,
   AppTrafficBySite,
   AppWidgetSummary,
-  AppCurrentSubject,
-  AppConversionRates,
 } from '../sections/@dashboard/app';
 
 // ----------------------------------------------------------------------
 
 export default function DashboardApp() {
-  const theme = useTheme();
+  
 
   return (
     <Page title="Dashboard">
@@ -33,7 +25,7 @@ export default function DashboardApp() {
 {/* ///////////////////////////////////////////////////////////////////////////////////////////////////////// */}
         <Grid container spacing={3}>
           <Grid item xs={12} sm={6} md={3}>
-            <AppWidgetSummary title="Weekly Sales" total={714000} icon={'ant-design:android-filled'} />
+            <AppWidgetSummary title="Weekly" total={714000} icon={'ant-design:android-filled'} />
           </Grid>
 
           <Grid item xs={12} sm={6} md={3}>
